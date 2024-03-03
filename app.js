2357c5a50de2b0710245c7f01b088b71529da711
r1407p <buszter0403@gmail.com>
2024-03-04 01:50:04 +0800

    feat: import module

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose';
+import { print, buildSchema } from 'graphql';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
+// Create an express app
 const app = express();
 const port = 5000;
 
-MONGO_URI=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+app.use(bodyParser.json());
+
+const MONGO_URI=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
